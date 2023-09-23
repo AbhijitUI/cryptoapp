73e87870fc36040a3feebfa9e7fe7f7f2dd65ae8
Abhijit Jadhav <abhijit.jadhav.0909@gmail.com>
2023-09-24 00:23:29 +0530

    Crypto App

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import store from "./app/store";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>
+);
